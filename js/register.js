b6661b6c14e8ed5dec107344d84427d2765da1d3
Igor Krasnov <krasnov.igor2@gmail.com>
2021-04-07 13:50:22 +0300

    Added order form

--- js/register.js
+++ js/register.js
@@ -1,32 +1,35 @@
 "use strict";
 
 document.addEventListener("DOMContentLoaded", () => {
-  
-  const data = [],
-    signInForm = document.querySelector("#signInForm"),
-    userName = document.querySelector("#userName"),
-    email = document.querySelector("#email"),
-    password = document.querySelector("#password"),
-		confirmPassword = document.querySelector("#confirm");
+	const data = [],
+		signInForm = document.querySelector("#signInForm"),
+		password = document.querySelector("#password"),
+		confirmPassword = document.querySelector("#confirm"),
+		inputs = document.querySelectorAll("input");
 
-  signInForm.addEventListener("submit", (event) => {
-    event.preventDefault();
-    const user = {
+	signInForm.addEventListener("submit", (event) => {
+		event.preventDefault();
+		const user = {
 			id: Date.now(),
-			name: userName.value,
-			email: email.value,
-			password: password.value,
 		};
-		
+
+		for (let i = 0; i < inputs.length; i++) {
+			const element = inputs[i],
+				elementId = element.getAttribute("id");
+
+			if (elementId !== "confirm" && elementId !== "submit-btn") {
+				user[elementId] = element.value;
+			} else continue;
+		}
+
 		if (confirmPassword.value !== password.value) {
-			alert('Passwords do not match');
+			alert("Passwords do not match");
 		} else {
 			data.push(user);
 			signInForm.reset();
 			console.log(data);
-	
-			localStorage.setItem('Users', JSON.stringify(data));
-			};
-  });
 
+			localStorage.setItem("Users", JSON.stringify(data));
+		}
+	});
 });
